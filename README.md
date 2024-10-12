9987348f2a485c1ffb3e542862b8893f2b37ee92
Nuno Henriques <nuno.henriques@izertislabs.pt>
2024-10-12 16:14:30 +0100

    Install daisyui and start homepage dev

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Next.js Tutorial for Beginners (User CRUD App)
 
 Based on youtube video: https://www.youtube.com/watch?v=iJejohAs9EY
+
+## Technologies
+
+- Next.js
+- Tailwind CSS
+- DaisyUI
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./global.css";
+import Link from "next/link";
 
 export const metadata = {
   title: "Next.js",
@@ -9,11 +10,30 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header>
-          <h3 className="text-blue-500">Haiku App</h3>
+        <header className="bg-gray-100 shadow-md">
+          <div className="container mx-auto">
+            <div className="navbar">
+              <div className="flex-1">
+                <Link href="/" className="btn btn-ghost text-xl">
+                  Haiku App
+                </Link>
+              </div>
+              <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                  <li>
+                    <Link href="/login">Log in</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </header>
-        {children}
-        <footer>footer</footer>
+        <main className="container mx-auto p-10">{children}</main>
+        <footer className="text-gray-400 text-center text-xs py-5">
+          <p>
+            Copyright &copy; {new Date().getFullYear()} - All rights reserved.
+          </p>
+        </footer>
       </body>
     </html>
   );
